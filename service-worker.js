0485a5078fb2adcfdbd1bfc6a6b68b59b04d502e
pinheiro767 <claudiapinheiro767@gmail.com>
2026-03-22 08:18:37 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,10 @@ const CACHE_NAME = 'roteiro-anatomia-pwa-v1';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
+  './style.css',
+  './app.js',
+  './data.js',
   './manifest.json',
-  './service-worker.js',
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
@@ -35,19 +37,12 @@ self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
 
   event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      return (
-        cachedResponse ||
-        fetch(event.request)
-          .then(networkResponse => {
-            const responseClone = networkResponse.clone();
-            caches.open(CACHE_NAME).then(cache => {
-              cache.put(event.request, responseClone);
-            });
-            return networkResponse;
-          })
-          .catch(() => caches.match('./index.html'))
-      );
-    })
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      });
+    }).catch(() => caches.match('./index.html'))
   );
 });
